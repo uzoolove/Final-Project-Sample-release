2e467c60973b2c4d0b16f443140e0661dce24963
uzoolove <uzoolove@gmail.com>
2025-07-07 09:21:23 +0000

    Update from https://github.com/FEBC-13/Final-Project-Sample/commit/ee92870cc5e2764c14a62824bf80405038aa4f62

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+# 레포지토리 상위 폴더로 이동
 cd ../
+# output 폴더 생성
 mkdir output
+# 레포지토리 하위의 모든 폴더와 파일을 output 하위로 복사(.gitignore, .github 같은 숨김 파일/폴더는 제외)
 cp -r ./Final-Project-Sample/* ./output
-cp -R ./output ./Final-Project-Sample/
+# output 폴더를 레포지토리 하위에 복사
+# cp -r ./output ./Final-Project-Sample/
+mv ./output ./Final-Project-Sample/
