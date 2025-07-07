13acb71cb9c535ad8a3d8a7960c0a54fee00435c
uzoolove <uzoolove@gmail.com>
2025-07-07 08:55:36 +0000

    Update from https://github.com/FEBC-13/Final-Project-Sample/commit/aecdc7332bccdec5594483c3b53d8902199157f8

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # FrontEnd BootCamp 13기 파이널 프로젝트 가이드
+* v.02
 
 ## 목차
 
---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd ../
+mkdir output
+cp -r ./Final-Project-Sample/* ./output
+cp -R ./output ./Final-Project-Sample/
